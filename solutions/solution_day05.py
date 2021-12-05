ea9365c7c638d350d20fc7ad42f5fdc084f98f62
Alexandros Rikos <alexrikos@gmail.com>
2021-12-05 14:42:28 +0100

    add solution of day 5

---
+++ solutions/solution_day05.py
@@ -0,0 +1,85 @@
+import logging
+import numpy as np
+
+logging.basicConfig(level=logging.DEBUG, 
+	format='%(asctime)s - %(levelname)s: %(message)s')
+
+# comment the next line to enable logging
+logging.disable(logging.CRITICAL)
+
+def get_input(filepath):
+	""" Reads the input from a file and stores it in a numpy array
+		
+		:param filepath: the path of the file that contains the input
+		:return instructions: a numpy array containing the offsets for each jump  
+	"""
+	with open(filepath) as f:
+		# use a numpy array, it is way faster than a python list
+		instructions = np.array([int(number) for number in f])
+
+	return instructions
+	
+
+def exit_instructions_maze(instructions, complex_rule=False):
+	""" Interpret the instructions contained in 'instructions' and exit the
+		maze of instructions.
+
+		There are two rules for the jumps
+		normal: After each jump, the offset of that instruction is incremented 
+		by one.
+		complex: After each jump, if the offset was three or more, the offset 
+		of that instruction is decremented by one. Else it is incremented by one.
+
+		:param instruction: a list containing the offsets for each jump
+		:param complex_rule: when it is True, the function uses the complex rule
+		:return steps: the number of steps it takes to exit
+		:return idx: the value of idx after exiting
+	"""
+	
+	idx = 0
+	steps = 0
+	length = len(instructions)
+
+	while idx < length and idx >= 0:
+		
+		jump = instructions[idx]
+		logging.debug(f"index: {idx}")
+		logging.debug(f"jump: {jump}")
+
+		if complex_rule == True:
+			if jump >= 3:
+				instructions[idx] -= 1
+			else: 
+				instructions[idx] += 1
+		else:
+			instructions[idx] += 1
+
+		# jump to the next index, count this jump
+		idx += jump
+		steps += 1
+
+	return steps
+
+def main():
+
+	choice = 'P2' # {'P1', 'P2'}
+
+	print(f'Running part {choice[-1]}')
+
+	# test input part1
+	assert exit_instructions_maze([0,3,0,1,-3]) == 5, 'Test input failed'
+	# test input part2
+	assert exit_instructions_maze([0,3,0,1,-3], True) == 10, 'Test input failed'
+
+	# get the input
+	instructions = get_input('../input/input_day05.txt')
+
+	if choice == 'P1':
+		steps = exit_instructions_maze(instructions)
+	else: 
+		steps = exit_instructions_maze(instructions, complex_rule=True)
+
+	print(f"Part {choice[-1]}: {steps} steps to reach the exit")
+
+if __name__ == '__main__':
+	main()
